1b54fbb571a1abf67c3236f5bd427aa13f7aef47
BugHunter-X <askzenai@gmail.com>
2024-12-25 06:58:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS configuration ...
+module.exports = {
+  // ... other configuration options ...
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // <-- This line is causing the error
+};
+```
